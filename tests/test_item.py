5e233a16481c8aa250c74c6bb89f1ef59683707f
LLiRRiK <kill2002@mail.ru>
2023-08-14 19:38:50 +0300

    homework_1 add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,30 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item_creation():
+    item = Item("Test Item", 10.0, 5)
+    assert item.name == "Test Item"
+    assert item.price == 10.0
+    assert item.quantity == 5
+
+
+# Тест на расчет общей стоимости товара
+def test_calculate_total_price():
+    item = Item("Test Item", 10.0, 5)
+    total_price = item.calculate_total_price()
+    assert total_price == 50.0
+
+
+# Тест на применение скидки
+def test_apply_discount():
+    item = Item("Test Item", 10.0, 5)
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8.0
+
+
+# Тест на добавление в общий список товаров
+def test_item_added_to_all():
+    item = Item("Test Item", 10.0, 5)
+    assert item in Item.all
