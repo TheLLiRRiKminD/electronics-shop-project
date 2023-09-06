06b9f4e8a0662ac81bc44ca318bd57585996d5d1
LLiRRiK <kill2002@mail.ru>
2023-09-06 14:28:16 +0300

    homework_4 0.1

---
+++ src/phone.py
@@ -0,0 +1,13 @@
+from src.item import Item
+
+
+class Phone(Item):
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim) -> None:
+        super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+
+    def __add__(self, other):
+        if not isinstance(other, Item):
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+        return self.quantity + other.quantity
