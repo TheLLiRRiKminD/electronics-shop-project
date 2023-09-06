368f13a1d85d9730ba34c5a0f698de690950d96f
LLiRRiK <kill2002@mail.ru>
2023-09-06 15:19:24 +0300

    homework_4 stable version 1

--- src/phone.py
+++ src/phone.py
@@ -7,7 +7,8 @@ class Phone(Item):
         super().__init__(name, price, quantity)
         self.number_of_sim = number_of_sim
 
-    def __add__(self, other):
-        if not isinstance(other, Item):
-            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
-        return self.quantity + other.quantity
+    def __setattr__(self, name, value):
+        if name == "number_of_sim" and value <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля.")
+        else:
+            self.__dict__[name] = value
